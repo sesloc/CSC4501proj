48e11fb334749132c6107bb8278dda1d1bf988b0
Spencer <sesloc@users.noreply.github.com>
2016-11-20 21:05:26 -0600

    Add files via upload

---
+++ ServerScript.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import socket, os, sys              #Imports
+
+sock = socket.socket()              #Create socket
+host = socket.gethostname()         #Get local machine name
+port = 80                           #Reserve port
+sock.bind((host, port))             #Bind to the port
+
+sock.listen(10)                     #Wait for connection
+while True:
+    c, addr = sock.accept()         #Establish connection
+    print("Connection to", addr)    #Print address of connection
+    c.send("connected")             #Send message
+    c.close()                       #Close connection
